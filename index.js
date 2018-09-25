e514ea114c3f5185825b53212bdfceb89ffb6e05
abitravers1989 <atravers4007@gmail.com>
2018-09-25 17:34:09 +0100

    middleware not working so commented out

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const bodyParser = require('body-parser');
 const app = express();
 //const emailRoute = require('./router/email')
 const logger = require('morgan');
-const readFileFunc = require('./middleware/readFileFunc')
+const readFileFunc = require('./middleware/readFileFunc');
+const errorhandling = require('./middleware/errorHandling');
 
 //console.log(area(4))
 
 app.use(logger('dev'))
 
 function clientErrorHandler(err, req, res, next) {
+    console.log(req.xhr)
     if (req.xhr) {
         res.status(500).send({ error: "something went wrong" })
     } else {
@@ -18,13 +20,13 @@ function clientErrorHandler(err, req, res, next) {
     }
 }
 
-function logErrors(err, req, res, next) {
-    console.error(err.stack)
-    next(err)
-}
+// function logErrors(err, req, res, next) {
+//     console.error(err.stack)
+//     next(err)
+// }
 
 app.use(clientErrorHandler);
-app.use(logErrors)
+//app.use(errorhandling.logErrors)
 
 app.get('/', function (req, res) {
     res.send('Working')
