48002653a01a40dd27fbdb5311d5ca4d2011d40b
abitravers1989 <atravers4007@gmail.com>
2018-10-15 18:05:40 +0100

    Remove warning about url Parser

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const fs = require('fs');
 //need to require this so can use .env variables
 const envs = require('dotenv').config();
 
+
+
 app.use(logger('dev'));
 app.use(errorhandling.clientErrorHandler);
 app.use(errorhandling.logErrors);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/health', function (req, res) {
     res.send('Working')
@@ -19,12 +23,9 @@ app.get('/health', function (req, res) {
     //console.log(res);
 })
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }));
-
 //sort end point: https://stackoverflow.com/questions/32883626/typeerror-app-use-requires-middleware-functions
-
 app.use('/api', userRoute)
+
 
 
 //setup log
@@ -45,31 +46,27 @@ accessLogStream.on('close', closedLog)
 
 
 
-//database connection
 
-//https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
-
-
-// const Database = require('./model/userModel');
-// const mongoose = require('mongoose');
-// const mongoDB = new Database("mongodb://localhost:27017", mongoose);
-// mongoDB.connectAndSetUpListners();
-
-const userName = process.env.USER_NAME
-const passWord = process.env.PASSWORD
-console.log(userName)
-console.log(passWord)
+//listen to mongo and startup the app.
+// const userName = process.env.USER_NAME
+// const passWord = process.env.PASSWORD
+// console.log(userName)
+// console.log(passWord)
 
 const mongoClient = require('mongodb').MongoClient
 const mongoConnectionString = `mongodb://${process.env.USER_NAME}:${process.env.PASSWORD}@ds135394.mlab.com:35394/simple-node-backend-app`
 
-mongoClient.connect(mongoConnectionString, (err, database) => {
+mongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, database) => {
     if (err) throw err
     const myDatabase = database.db('simple-node-backend-app')
-    myDatabase.collection('users').find().toArray((err, result) => {
-        if (err) throw err;
-        console.log(result)
-    })
+    //move this into get endpooint
+    // myDatabase.collection('users').find().toArray((err, result) => {
+    //     if (err) throw err;
+    //     console.log(result)
+    // })
+    const printPort = (portNumber) => { console.log(`listening on port ${portNumber}`) }
+    const port = 3000
+    app.listen(port, printPort(port))
 })
 
 //https://zellwk.com/blog/crud-express-mongodb/
@@ -103,7 +100,3 @@ mongoClient.connect(mongoConnectionString, (err, database) => {
 //spec.composeModel(user)
 
 module.exports = app;
-
-const printPort = (portNumber) => { console.log(`listening on port ${portNumber}`) }
-const port = 3000
-app.listen(port, printPort(port))
