c217d457817e5821fb9f967ba458591e00250dc3
abitravers1989 <atravers4007@gmail.com>
2018-10-30 16:58:51 +0000

    promisifying mongo connection

--- repository/mongo.js
+++ repository/mongo.js
@@ -1,118 +1,30 @@
-// const { MongoClient } = require('mongodb');
-// const mongoConnectionString = `mongodb://${process.env.USER_NAME}:${process.env.PASSWORD}@ds135394.mlab.com:35394/simple-node-backend-app`
+const { MongoClient } = require('mongodb');
+const mongoConnectionString = `mongodb://${process.env.USER_NAME}:${process.env.PASSWORD}@ds135394.mlab.com:35394/simple-node-backend-app`
 // const mongoConnectionString = 'mongodb://localhost/       REMOVE?nodeBackend';
 
-module.exports = {
-    connect: async (MongoClient, mongoConnectionString) => {
-        //console.log(process.env.USER_NAME)
-        if (!process.env.USER_NAME && !process.env.PASSWORD) {
-            throw console.log("Mongo user name and password is not provided.")
-        }
-        const client = await MongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, db) => {
-            if (err) return reject(console.log(err))
-            console.log('Mongo on');
-            //resolve()
-            //db.close;
-        }).catch(() => { });
-        const database = client.db('simple-node-backend-app');
-        console.log(`mongo db ${database}`)
-        return database;
+//module.exports = {
+const connect = (MongoClient, mongoConnectionString) => {
+    //console.log(process.env.USER_NAME)
+    if (!process.env.USER_NAME && !process.env.PASSWORD) {
+        throw console.log("Mongo user name and password is not provided.")
     }
 
+    new Promise((resolve, reject) => {
+        return MongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, db) => {
+            if (err) return reject(console.log(err))
+            const database = db.db('simple-node-backend-app');
+            console.log(`Mongo now listening. Mongo Object: ${database.hasOwnProperty("collection")}`);
+            database.collection('users').find()
+            return resolve(database)
+        })
+    })
 }
 
 
-
-
-
-// const thing = MongoClient.connect
-
-// console.log(new thing.MongoError());
-
-
-
-
-
-
-// module.exports = {
-//     connect: async (mongoClient, mongoConnectionString) => {
-//         try {
-//             let client = await mongoClient.connect(mongoConnectionString, { useNewUrlParser: true })
-//             let db = client.db('simple-node-backend-app');
-//             try {
-//                 const res = await db.collection('users').updateOne({
-//                     "email": 'test@test.com'
-//                 }, { $set: someObj }, { upsert: true })
-//                 console.log(`res => ${JSON.stringify(res)}`)
-//             }
-//             catch {
-//                 (err => console.log(err))
-//             }
-//             finally {
-//                 client.close();
-//             }
-//         }
-//     }
-        //catch { console.log(err) }
-    //}
-
-    // .catch(err => console.log(err))
 //}
 
+connect(MongoClient, mongoConnectionString)
 
 
 
-// module.exports = class mongoDb {
-//     constructor(mongoClient, mongoConnectionString) {
-//         this.mongoClient = mongoClient;
-//         this.mongoConnectionString = mongoConnectionString;
-//         let myDatabase;
-//     }
-//     connect() {
-//         this.mongoClient.connect(this.mongoConnectionString, { useNewUrlParser: true }, (err, database) => {
-//             if (err) throw err
-//             mongoDb.myDatabase = database.db('simple-node-backend-app')
-//             console.log('Mongo on');
-//             //console.log(mongoDb.myDatabase)
-//             //return this;
-//         })
-//     }
-//     //sync or async?
-//     getAll() {
-//         console.log(mongoDb.myDatabase)
-//         mongoDb.myDatabase.collection('users').find().toArray((err, result) => {
-//             if (err) throw err;
-//             console.log(result)
-//         })
-//     }
-// }
-    // (async () => {
-    //     let client = await MongoClient.connect(connectionString,
-    //         { useNewUrlParser: true });
-
-    //     let db = client.db('dbName');
-    //     try {
-    //        const res = await db.collection("collectionName").updateOne({ 
-    //            "someKey": someValue
-    //        }, { $set: someObj }, { upsert: true });
-
-    //        console.log(`res => ${JSON.stringify(res)}`);
-    //     }
-    //     finally {
-    //         client.close();
-    //     }
-    // })()
-    //     .catch(err => console.error(err));
-
-
-// {
-//     connect: function (mongoClient, mongoConnectionString) {
-//         mongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, database) => {
-//             if (err) throw err
-//             const myDatabase = database.db('simple-node-backend-app')
-//             console.log('Mongo on');
-//             console.log(myDatabase);
-//         })
-//     }
-// }
-
+//alternative would be to find a mongo libary that uses async and await and promises 
