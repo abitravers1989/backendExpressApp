4ba87ccb8a7848abb0bcb13fbadb48041f35b836
abitravers1989 <atravers4007@gmail.com>
2018-11-03 15:26:56 +0000

    tried to remove promises to see if that works

--- repository/mongo.js
+++ repository/mongo.js
@@ -1,66 +1,27 @@
-const MongoClient = require('mongodb');
-const mongoConnectionString = 'mongodb://localhost/';
-
-
-const mongDatabase = (MongoClient, mongoConnectionString) => {
-    var database;
+const connect = (MongoClient, mongoConnectionString) => {
+    // if (!process.env.USER_NAME && !process.env.PASSWORD) {
+    //     throw console.log("Mongo user name and password is not provided.")
+    // }
     try {
-        if (!process.env.USER_NAME && !process.env.PASSWORD) {
-                throw console.log("Mongo user name and password is not provided.")
-            } 
-            MongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, db) => {
-                if (err) return reject(console.log(err))
-                .then(db.db('simple-node-backend-app'))
-                .then(console.log(`Mongo now listening. Mongo Object: ${database}`))
-                .then(database.collection('users').find())
-                // database = db.db('simple-node-backend-app');
-                // console.log(`Mongo now listening. Mongo Object: ${database}`);
-                //return database;
-            })   
+        // new Promise((resolve, reject) => {
+        MongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, db) => {
+            if (err) return console.log(err)
+            // reject(console.log(err))
+            const database = db.db('nodeBackend');
+            console.log(`Mongo now listening. Mongo Object: ${database}`);
+            const users = database.collection('users').find();
+            //console.log(users)
+            return users
+            return resolve(users)
+        })
+        // })
     } catch (e) {
         return new Error(`issue connecting to mongo: ${e}`)
     }
-    //
-    return database.collection('users').find();
+
 }
 
-mongDatabase(MongoClient, mongoConnectionString)
-
-
-// const { MongoClient } = require('mongodb');
-// //const mongoConnectionString = `mongodb://${process.env.USER_NAME}:${process.env.PASSWORD}@ds135394.mlab.com:35394/simple-node-backend-app`
-// const mongoConnectionString = 'mongodb://localhost/';
-
-// //module.exports = {
-// const connect = (MongoClient, mongoConnectionString) => {
-//     if (!process.env.USER_NAME && !process.env.PASSWORD) {
-//         throw console.log("Mongo user name and password is not provided.")
-//     }
-//     try {
-//         new Promise((resolve, reject) => {
-//             MongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, db) => {
-//                 if (err) return reject(console.log(err))
-//                 const database = db.db('simple-node-backend-app');
-//                 console.log(`Mongo now listening. Mongo Object: ${database}`);
-//                 return resolve(database)
-//             })
-//         })
-//     } catch (e) {
-//         return new Error(`issue connecting to mongo: ${e}`)
-//     }
-//  }
-
-// //maybe make the connection emit an event instead then act on that event?
-
-// const showError = () => {
-//     console.log("Error with mongoConnectionPromise")
-// }
-
-// const mongoConnectionPromise = connect(MongoClient, mongoConnectionString)
-// mongoConnectionPromise.then((database) => {
-//     const users = database.collection('users').find();
-//     console.log(users)
-// })
-// mongoConnectionPromise.catch(showError)
-
-//alternative would be to find a mongo libary that uses async and await and promises 
+const { MongoClient } = require('mongodb');
+const mongoConnectionString = 'mongodb://localhost/';
+//const mongoConnectionString = `mongodb://${process.env.USER_NAME}:${process.env.PASSWORD}@ds135394.mlab.com:35394/simple-node-backend-app`
+console.log(connect(MongoClient, mongoConnectionString))
--- repository/mongo2.js
+++ repository/mongo2.js
@@ -16,9 +16,78 @@ const connect = (MongoClient, mongoConnectionString) => {
         return new Error(`issue connecting to mongo: ${e}`)
     }
     return database.collection('users').find()
- }
+}
 
- const MongoClient = require('mongodb');
+const MongoClient = require('mongodb');
 const mongoConnectionString = 'mongodb://localhost/';
 
 connect(MongoClient, mongoConnectionString)
+
+
+
+const MongoClient = require('mongodb');
+const mongoConnectionString = 'mongodb://localhost/';
+
+
+// const mongDatabase = (MongoClient, mongoConnectionString) => {
+//     var database;
+//     try {
+//         if (!process.env.USER_NAME && !process.env.PASSWORD) {
+//                 throw console.log("Mongo user name and password is not provided.")
+//             } 
+//             MongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, db) => {
+//                 if (err) return reject(console.log(err))
+//                 .then(db.db('simple-node-backend-app'))
+//                 .then(console.log(`Mongo now listening. Mongo Object: ${database}`))
+//                 .then(database.collection('users').find())
+//                 // database = db.db('simple-node-backend-app');
+//                 // console.log(`Mongo now listening. Mongo Object: ${database}`);
+//                 //return database;
+//             })   
+//     } catch (e) {
+//         return new Error(`issue connecting to mongo: ${e}`)
+//     }
+//     //
+//     return database.collection('users').find();
+// }
+
+// mongDatabase(MongoClient, mongoConnectionString)
+
+
+// const { MongoClient } = require('mongodb');
+// //const mongoConnectionString = `mongodb://${process.env.USER_NAME}:${process.env.PASSWORD}@ds135394.mlab.com:35394/simple-node-backend-app`
+// const mongoConnectionString = 'mongodb://localhost/';
+
+// //module.exports = {
+// const connect = (MongoClient, mongoConnectionString) => {
+//     if (!process.env.USER_NAME && !process.env.PASSWORD) {
+//         throw console.log("Mongo user name and password is not provided.")
+//     }
+//     try {
+//         new Promise((resolve, reject) => {
+//             MongoClient.connect(mongoConnectionString, { useNewUrlParser: true }, (err, db) => {
+//                 if (err) return reject(console.log(err))
+//                 const database = db.db('simple-node-backend-app');
+//                 console.log(`Mongo now listening. Mongo Object: ${database}`);
+//                 return resolve(database)
+//             })
+//         })
+//     } catch (e) {
+//         return new Error(`issue connecting to mongo: ${e}`)
+//     }
+//  }
+
+// //maybe make the connection emit an event instead then act on that event?
+
+// const showError = () => {
+//     console.log("Error with mongoConnectionPromise")
+// }
+
+// const mongoConnectionPromise = connect(MongoClient, mongoConnectionString)
+// mongoConnectionPromise.then((database) => {
+//     const users = database.collection('users').find();
+//     console.log(users)
+// })
+// mongoConnectionPromise.catch(showError)
+
+//alternative would be to find a mongo libary that uses async and await and promises 
